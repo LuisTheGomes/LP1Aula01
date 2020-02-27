c1db0f205201fe5a8fde98f86e7d464bd96e42e9
Luís Gomes <afonsofgomes@gmail.com>
2020-02-27 10:54:45 +0000

    First commit

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero=1;
+            uint dank=2U;
+
+            Console.WriteLine(numero);
+            Console.WriteLine(dank);
+        }
+    }
+}
