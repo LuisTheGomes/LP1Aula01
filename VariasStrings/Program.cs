bf46d6253b0ca3d90a4dd54a206a279dbad217a4
Luís Gomes <afonsofgomes@gmail.com>
2020-02-27 11:39:22 +0000

    First Commit

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string laugh = "ahahahah";
+            string cringe = "yikes";
+            string verbatim = @"\barras";
+            string aspas = @"aspas""aspas";
+
+            Console.WriteLine(laugh);
+            Console.WriteLine(cringe);
+            Console.WriteLine(verbatim);
+            Console.WriteLine(aspas);
+        }
+    }
+}
