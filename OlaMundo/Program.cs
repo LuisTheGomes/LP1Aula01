69c21e3a526be5856aa8e27d69080cc359a262e2
Luís Gomes <afonsofgomes@gmail.com>
2020-02-27 10:30:14 +0000

    Changed documentation

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,12 +2,18 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// Main program
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Function
+        /// </summary>
+        /// <param name="args">lmao</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
             Console.WriteLine("Bye World");
         }
     }
